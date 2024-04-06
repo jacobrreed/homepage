86789a50d9324c745073324a3e440d34f7467ec4
jacobrreed <jacob.russell.reed@gmail.com>
2024-04-05 20:51:01 -0400

    SEO optimizations

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,12 @@
+/* eslint-disable @next/next/no-title-in-document-head */
 import { Html, Head, Main, NextScript } from "next/document";
-
 export default function Document() {
   return (
     <Html lang="en">
-      <Head
-      title="Jacob Reed"
-      >
+      <Head title="Jacob Reed">
         <link rel="shortcut icon" href="/favicon.ico" />
         <meta name="description" content="Jacob Reed's personal website" />
+        <title>Jacob Reed</title>
       </Head>
       <body>
         <Main />
